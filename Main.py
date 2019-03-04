b5d30dc8285243a3d27e6a75406f2cc3ec5c866b
Valeriy Ivannikov <v.ivannikov@space307.com>
2019-03-04 12:40:09 +0300

    req

--- Main.py
+++ Main.py
@@ -1,18 +1,26 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
-from telegram.ext import Updater, CommandHandler
 
 token = "778249680:AAGdQFShvI2jxoWvs4HWq1uTpPkw9uCTIE8"
 
-
-def start(update, context):
-    logger.warning("start")
-    update.message.reply_text(text="I'm a bot, please talk to me!")
-
+updater = Updater(token=token)  # Токен API к Telegram
+dispatcher = updater.dispatcher
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
+
+
+# Обработка команд
+def text_message(bot, update):
+    response = 'Получил Ваше сообщение: ' + update.message.text
+    bot.send_message(chat_id=update.message.chat_id, text=response)
+
+
+def start(bot, update):
+    logger.warning("start")
+    bot.send_message(chat_id=update.message.chat_id, text='Привет, давай пообщаемся?')
 
 
 def error(update, context):
@@ -22,13 +30,16 @@ def error(update, context):
 
 def main():
     logger.warning("main")
-    updater = Updater(token=token)
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler('start', start))
 
-    dispatcher.add_error_handler(error)
-
-    updater.start_polling()
+    # Хендлеры
+    start_command_handler = CommandHandler('start', start)
+    text_message_handler = MessageHandler(Filters.text, text_message)
+    # Добавляем хендлеры в диспетчер
+    dispatcher.add_handler(start_command_handler)
+    dispatcher.add_handler(text_message_handler)
+    # Начинаем поиск обновлений
+    updater.start_polling(clean=True)
+    # Останавливаем бота, если были нажаты Ctrl + C
     updater.idle()
 
 
