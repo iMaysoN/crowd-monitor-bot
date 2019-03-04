543a5f357ecdd12b4b4ee1b83df3265dd8b939eb
Valeriy Ivannikov <v.ivannikov@space307.com>
2019-03-04 11:44:31 +0300

    first

---
+++ Main.py
@@ -0,0 +1,34 @@
+import logging
+from telegram.ext import Updater, CommandHandler
+
+token = "778249680:AAGdQFShvI2jxoWvs4HWq1uTpPkw9uCTIE8"
+
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+    updater = Updater(token=token)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler('start', start))
+
+    dispatcher.add_error_handler(error)
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
